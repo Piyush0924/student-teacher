1b1cd91db1cd8963d64f4a37d02f1321659f4b6e
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2025-01-17 08:05:59 +0530

    keeping as it is

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Vite configuration
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://vercel.com/piyushs-projects-c005e3cc/student-teacher/7AW33wJcHbmq6j61qMLuHhGUtvnw', // Backend server URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+})
